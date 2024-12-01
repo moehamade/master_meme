147c42c6b947117920e8838b3e35d68700f2bb99
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-01 18:53:10 +0200

    feat: Define custom color palette
    Defined a custom color palette with solid colors and gradient colors for the app's theme. This replaces the previous default colors.

--- app/src/main/java/com/mobilecampus/mastermeme/ui/theme/Color.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.mobilecampus.mastermeme.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//Solid colors
+val Onyx = Color(0xFF0F0D13)
+val Mirage = Color(0xFF1D1B20)
+val BalticSea = Color(0xFF2B2930)
+val Boulder = Color(0xFF79747E)
+val ButterflyBush = Color(0xFF65558F)
+val Lavender = Color(0xFFCCC2DC)
+val LavenderPinocchio = Color(0xFFE6E0E9)
+val LavenderPink = Color(0xFFEADDFF)
+val LavenderMist = Color(0xFFECE6F0)
+val CornellRed = Color(0xFFB3261E)
+val CherryPie = Color(0xFF21005D)
+
+//Gradient solid colors
+val LightViolet = Color(0xFFD0BCFE)
+val PaleLilac = Color(0xFFE0D0FA)
+val LightLavender = Color(0xFFAD90F1)
+
+
+//Default
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
