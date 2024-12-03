3a8a8beee4a9a92d454d97ae5ca546d3178d9609
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-03 18:26:46 +0200

    feat: Define custom color palette
    Defined a custom color palette with solid colors and gradient colors for the app's theme. This replaces the previous default colors.

--- app/src/main/java/com/mobilecampus/mastermeme/ui/theme/Color.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/ui/theme/Color.kt
@@ -1,27 +1,41 @@
 package com.mobilecampus.mastermeme.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 
 //Solid colors
-val Onyx = Color(0xFF0F0D13)
-val Mirage = Color(0xFF1D1B20)
-val BalticSea = Color(0xFF2B2930)
-val Boulder = Color(0xFF79747E)
-val ButterflyBush = Color(0xFF65558F)
-val Lavender = Color(0xFFCCC2DC)
-val LavenderPinocchio = Color(0xFFE6E0E9)
-val LavenderPink = Color(0xFFEADDFF)
-val LavenderMist = Color(0xFFECE6F0)
-val CornellRed = Color(0xFFB3261E)
-val CherryPie = Color(0xFF21005D)
+internal val Onyx = Color(0xFF0F0D13)
+internal val Mirage = Color(0xFF1D1B20)
+internal val BalticSea = Color(0xFF2B2930)
+internal val Boulder = Color(0xFF79747E)
+internal val ButterflyBush = Color(0xFF65558F)
+internal val Lavender = Color(0xFFCCC2DC)
+internal val LavenderPinocchio = Color(0xFFE6E0E9)
+internal val LavenderPink = Color(0xFFEADDFF)
+internal val LavenderMist = Color(0xFFECE6F0)
+internal val CornellRed = Color(0xFFB3261E)
+internal val CherryPie = Color(0xFF21005D)
 
 //Gradient solid colors
-val LightViolet = Color(0xFFD0BCFE)
-val PaleLilac = Color(0xFFE0D0FA)
-val LightLavender = Color(0xFFAD90F1)
+internal val LightViolet = Color(0xFFD0BCFE)
+internal val PaleLilac = Color(0xFFE0D0FA)
+internal val LightLavender = Color(0xFFAD90F1)
+
+// Gradient brushes
+internal val ButtonDefaultGradient = Brush.horizontalGradient(
+    colors = listOf(
+        LavenderPink, LightViolet
+    )
+)
+
+internal val ButtonPressedGradient = Brush.horizontalGradient(
+    colors = listOf(
+        PaleLilac, LightLavender
+    )
+)
 
 
 //Default
-val White = Color(0xFFFFFFFF)
-val Black = Color(0xFF000000)
+internal val White = Color(0xFFFFFFFF)
+internal val Black = Color(0xFF000000)
