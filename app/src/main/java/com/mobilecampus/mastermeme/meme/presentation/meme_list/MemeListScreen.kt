725aceb0e373756c106df9b9281cb3ef2f7f440b
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-07 18:49:05 +0200

    feat: Implemented empty state for meme list screen
    Added an empty state UI to the meme list screen, displayed when there are no memes.

--- app/src/main/java/com/mobilecampus/mastermeme/meme/presentation/meme_list/MemeListScreen.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/meme/presentation/meme_list/MemeListScreen.kt
@@ -1,20 +1,29 @@
 package com.mobilecampus.mastermeme.meme.presentation.meme_list
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.mobilecampus.mastermeme.R
 import com.mobilecampus.mastermeme.meme.presentation.meme_list.components.MemeListTopAppBar
 import com.mobilecampus.mastermeme.meme.presentation.meme_list.components.SortOption
 
@@ -48,8 +57,26 @@ fun MemeListScreen() {
             }
         }
     ) { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-            // Content of the screen
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_empty_meme),
+                contentDescription = "Empty state",
+                modifier = Modifier
+            )
+            Text(
+                text = "Tap + button to create your first meme",
+                modifier = Modifier.padding(top = 34.dp),
+                style = MaterialTheme.typography.bodySmall.copy(
+                    color = MaterialTheme.colorScheme.outline,
+                    textAlign = TextAlign.Center
+                )
+            )
         }
     }
 }
