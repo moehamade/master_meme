1d16ad8861d32c9f9798be671b9cabc674a240f0
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-08 14:30:24 +0200

    feat: Initialized Koin and declared modules for dependency injection.
    This includes initializing Koin in the `onCreate` method of `App` and declaring the `memeDataModule`, `domainModule`, and `presentationModule` as Koin modules. It also sets the Android logger level to ERROR.

--- app/src/main/java/com/mobilecampus/mastermeme/App.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/App.kt
@@ -1,19 +1,21 @@
 package com.mobilecampus.mastermeme
 
 import android.app.Application
-import com.mobilecampus.mastermeme.di.viewModelsModule
+import com.mobilecampus.mastermeme.meme.data.di.memeDataModule
+import com.mobilecampus.mastermeme.meme.domain.di.domainModule
+import com.mobilecampus.mastermeme.meme.presentation.di.presentationModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
         startKoin {
+            androidLogger(Level.ERROR)
             androidContext(this@App)
-            modules(
-                viewModelsModule
-            )
+            modules(listOf(memeDataModule, domainModule, presentationModule))
         }
     }
 }
