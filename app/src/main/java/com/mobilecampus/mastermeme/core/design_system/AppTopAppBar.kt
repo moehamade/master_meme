e2a68e0e39bd3d694c3bc6a9a1c80deba9a37fa4
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-08 13:04:41 +0200

    Refactored UI components:
    - Removed unused imports and properties in `GradientFilledButton`, `AppTopAppBar` and `Color`.
    - Simplified the structure and implementation of UI components.

--- app/src/main/java/com/mobilecampus/mastermeme/core/design_system/AppTopAppBar.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/core/design_system/AppTopAppBar.kt
@@ -1,17 +1,13 @@
 package com.mobilecampus.mastermeme.core.design_system
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
