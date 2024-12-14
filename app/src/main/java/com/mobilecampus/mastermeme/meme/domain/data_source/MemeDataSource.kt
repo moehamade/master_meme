7188caecc36942b8cab1e9de46fc6e6550833756
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-14 10:53:14 +0200

    Refactor: Update Meme model and data source to use MemeItem- Updated the `Meme` model to `MemeItem` and introduced different meme types. - Modified the `MemeDataSource` to use `MemeItem` instead of `Meme`. - Updated the data source functions to handle `MemeItem.ImageMeme` specifically.

--- app/src/main/java/com/mobilecampus/mastermeme/meme/domain/data_source/MemeDataSource.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/meme/domain/data_source/MemeDataSource.kt
@@ -1,13 +1,13 @@
 package com.mobilecampus.mastermeme.meme.domain.data_source
 
 import android.graphics.Bitmap
-import com.mobilecampus.mastermeme.meme.domain.model.Meme
+import com.mobilecampus.mastermeme.meme.domain.model.MemeItem
 import kotlinx.coroutines.flow.Flow
 
 interface MemeDataSource {
-    fun getMemes(): Flow<List<Meme>>
-    suspend fun saveMeme(meme: Meme)
-    suspend fun deleteMeme(meme: Meme)
+    fun getMemes(): Flow<List<MemeItem.ImageMeme>>
+    suspend fun saveMeme(meme: MemeItem.ImageMeme)
+    suspend fun deleteMeme(meme: MemeItem.ImageMeme)
     suspend fun toggleFavorite(memeId: Int)
     suspend fun getTemplates(): List<String>
     suspend fun saveMemeImage(bitmap: Bitmap): String
