435591f12393ee9c75dcbdd235ef3b6f4733e98e
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-17 09:01:05 +0200

    feat: Implement File Sharing- Added `shareFiles` function to `FileManager` to share multiple files with other applications. - Implemented `getUriFromPath` to get the content URI for a file path, necessary for sharing files with other apps. - Updated `FileManagerImpl` to utilize content URIs for sharing files. - The `shareFiles` function creates an `ACTION_SEND_MULTIPLE` intent with the content URIs of the files to be shared and launches a chooser to allow the user to select an app for sharing.

--- app/src/main/java/com/mobilecampus/mastermeme/meme/data/local/util/FileManagerImpl.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/meme/data/local/util/FileManagerImpl.kt
@@ -1,10 +1,16 @@
 package com.mobilecampus.mastermeme.meme.data.local.util
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
+import androidx.core.content.FileProvider
 import com.mobilecampus.mastermeme.meme.domain.util.FileManager
 import java.io.File
 
-class FileManagerImpl : FileManager {
+class FileManagerImpl(
+    private val context: Context
+) : FileManager {
     override suspend fun deleteFiles(uris: Set<String>) {
         uris.forEach { uri ->
             try {
@@ -16,5 +22,32 @@ class FileManagerImpl : FileManager {
                 Log.e("FileManager", "Error deleting file: $uri", e)
             }
         }
+    }
+    override suspend fun shareFiles(uris: Set<String>) {
+        try {
+            val shareUris = uris.map { getUriFromPath(it) }
+
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND_MULTIPLE
+                type = "image/*"
+                putParcelableArrayListExtra(Intent.EXTRA_STREAM, ArrayList(shareUris))
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+
+            val chooserIntent = Intent.createChooser(shareIntent, "Share Memes")
+            chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(chooserIntent)
+        } catch (e: Exception) {
+            Log.e("FileManager", "Error sharing files", e)
+            throw e
+        }
+    }
+
+    override fun getUriFromPath(path: String): Uri {
+        return FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.provider",
+            File(path)
+        )
     }
 }
--- app/src/main/java/com/mobilecampus/mastermeme/meme/domain/util/FileManager.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/meme/domain/util/FileManager.kt
@@ -1,5 +1,10 @@
 package com.mobilecampus.mastermeme.meme.domain.util
 
+import android.content.Context
+import android.net.Uri
+
 interface FileManager {
     suspend fun deleteFiles(uris: Set<String>)
+    suspend fun shareFiles(uris: Set<String>)
+    fun getUriFromPath(path: String): Uri
 }
