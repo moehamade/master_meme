69288ceee66fecafa81aa83e29ecc7cf642334d1
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-22 20:01:38 +0200

    removed imports

--- app/src/main/java/com/mobilecampus/mastermeme/core/presentation/design_system/AppTopAppBar.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/core/presentation/design_system/AppTopAppBar.kt
@@ -1,7 +1,6 @@
 package com.mobilecampus.mastermeme.core.presentation.design_system
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -10,7 +9,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
