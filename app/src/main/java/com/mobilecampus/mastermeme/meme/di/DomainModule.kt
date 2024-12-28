f7c53705eb8bcdb96377c2bca9a536f109b64a50
Moe Hamade <mohamadhamade30@gmail.com>
2024-12-29 00:03:14 +0200

    feat: Add ShareTemporaryMeme use case and MemeRenderer
    - Added `ShareTemporaryMemeImpl` as an implementation of `ShareTemporaryMeme`.
    - Added `MemeRenderer` as a singleton.
    - Modified `SaveMemeUseCaseImpl` to depend on `MemeRenderer`.

--- app/src/main/java/com/mobilecampus/mastermeme/meme/di/DomainModule.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/meme/di/DomainModule.kt
@@ -5,20 +5,25 @@ import com.mobilecampus.mastermeme.meme.data.local.use_case.GetMemesUseCaseImpl
 import com.mobilecampus.mastermeme.meme.data.local.use_case.GetTemplatesUseCaseImpl
 import com.mobilecampus.mastermeme.meme.data.local.use_case.SaveMemeUseCaseImpl
 import com.mobilecampus.mastermeme.meme.data.local.use_case.ShareMemesUseCaseImpl
+import com.mobilecampus.mastermeme.meme.data.local.use_case.ShareTemporaryMemeImpl
 import com.mobilecampus.mastermeme.meme.data.local.use_case.ToggleFavoriteUseCaseImpl
+import com.mobilecampus.mastermeme.meme.domain.MemeRenderer
 import com.mobilecampus.mastermeme.meme.domain.use_case.DeleteMemeUseCase
 import com.mobilecampus.mastermeme.meme.domain.use_case.GetMemesUseCase
 import com.mobilecampus.mastermeme.meme.domain.use_case.GetTemplatesUseCase
 import com.mobilecampus.mastermeme.meme.domain.use_case.SaveMemeUseCase
 import com.mobilecampus.mastermeme.meme.domain.use_case.ShareMemesUseCase
+import com.mobilecampus.mastermeme.meme.domain.use_case.ShareTemporaryMeme
 import com.mobilecampus.mastermeme.meme.domain.use_case.ToggleFavoriteUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
     factory<GetMemesUseCase> { GetMemesUseCaseImpl(get()) }
     factory<ToggleFavoriteUseCase> { ToggleFavoriteUseCaseImpl(get()) }
-    factory<DeleteMemeUseCase> { DeleteMemeUseCaseImpl(get(),get()) }
+    factory<DeleteMemeUseCase> { DeleteMemeUseCaseImpl(get(), get()) }
     factory<GetTemplatesUseCase> { GetTemplatesUseCaseImpl(get()) }
-    factory<SaveMemeUseCase> { SaveMemeUseCaseImpl(get(), get())}
+    factory<SaveMemeUseCase> { SaveMemeUseCaseImpl(get(), get(), get()) }
+    single<ShareTemporaryMeme> { ShareTemporaryMemeImpl(get(), get(), get()) }
     single<ShareMemesUseCase> { ShareMemesUseCaseImpl(get(), get()) }
+    single { MemeRenderer(get()) }
 }
