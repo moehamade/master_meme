fe27e8f6a4f50a762400028e286373ea30ce1792
Moe Hamade <mohamadhamade30@gmail.com>
2025-01-02 19:10:56 +0200

    refactor: Migrate dependency injection to Koin DSL
    - Migrated dependency injection to use the Koin DSL syntax.
    - Changed `factory` and `single` to `factoryOf` and `singleOf` respectively, for more concise declarations.
    - Used `bind` to explicitly define the interface implementations.

--- app/src/main/java/com/mobilecampus/mastermeme/meme/di/PresentationModule.kt
+++ app/src/main/java/com/mobilecampus/mastermeme/meme/di/PresentationModule.kt
@@ -1,11 +1,14 @@
 package com.mobilecampus.mastermeme.meme.di
 
+import com.mobilecampus.mastermeme.meme.presentation.screens.editor.util.MemeRenderer
 import com.mobilecampus.mastermeme.meme.presentation.screens.editor.MemeEditorViewModel
 import com.mobilecampus.mastermeme.meme.presentation.screens.meme_list.MemeListViewModel
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val presentationModule = module {
     viewModelOf(::MemeListViewModel)
     viewModelOf(::MemeEditorViewModel)
+    singleOf(::MemeRenderer)
 }
